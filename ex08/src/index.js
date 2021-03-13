eb92037d7c3e4a367572a457b0d0d723c63796cb
Eskodevelop <esmirneretljak@gmail.com>
2021-03-13 10:49:09 +0100

    Day05

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line 
+    var lastName = "Doe";
+    var lastNameLength = lastName.Length
+
+    // Only change code above this line 
+    return lastName.length;
+
+};
+
+console.log(main());
+module.exports = main;
---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line 
+    var lastName = "Doe";
+    var firstLetterOfLAstName = lastName[0]
+
+
+    // Only change code above this line 
+    return firstLetterOfLAstName;
+
+};
+
+console.log(main());
+module.exports = main;
